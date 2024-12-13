d1b4c4007b228d76d68c02aa63d4e2f7c5cdccad
Sohilaayman129 <solyayman45@gmail.com>
2024-12-13 02:24:07 +0200

    +normalLevel

--- src/main/java/com/example/fx_test/HelloApplication.java
+++ src/main/java/com/example/fx_test/HelloApplication.java
@@ -21,7 +21,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage)  {
 try {
-    URL url = new File("C:\\Users\\Ahmed Sobeah\\dev\\fx_test\\src\\main\\java\\com\\example\\fx_test\\Scene1.fxml").toURI().toURL();
+    URL url = new File("C:\\Users\\hp\\IdeaProjects\\mastermindgame\\src\\main\\java\\com\\example\\fx_test\\Scene1.fxml").toURI().toURL();
     Parent root = FXMLLoader.load(url);
     Scene scene = new Scene(root);
     stage.setScene(scene);
@@ -34,6 +34,8 @@ try {
 
 
     }
+
+
 
     public static void main(String[] args) {
         launch();
